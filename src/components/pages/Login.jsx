76065319071fbfb74077507d6eb3ee7703193d12
unknown <yazid1bernou@gmail.com>
2025-12-16 22:39:19 +0100

    create login page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,38 @@
+import Layout from "../common/Layout"
+import { Link } from "react-router-dom";
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <Layout>
+            <div className='container py-5 mt-5'>
+                <div className='d-flex align-items-center justify-content-center'>
+                    <form>
+                        <div className='card border-0 shadow login'>
+                            <div className='card-body p-4'>
+                                <h3 className='border-bottom pb-3 mb-3'>Login</h3>
+                                <div className='mb-3'>
+                                    <label className='form-label' htmlFor="email">Email</label>
+                                    <input
+                                        type="text" className='form-control' placeholder='Email' />
+                                </div>
 
-        </div>
+                                <div className='mb-3'>
+                                    <label className='form-label' htmlFor="password">Password</label>
+                                    <input
+                                        type="password" className='form-control'
+                                        placeholder='Password' />
+
+                                </div>
+
+                                <div className='d-flex justify-content-between align-items-center'>
+                                    <button className='btn btn-primary'>Login</button>
+                                    <Link to={`/account/register`} className='text-secondary'>Register Here</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
