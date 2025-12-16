dc1d31f3ddf7300a01c38bc575eba62935c4b3e8
unknown <yazid1bernou@gmail.com>
2025-12-16 22:48:53 +0100

    create register page

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,9 +1,53 @@
+import { Link } from "react-router-dom"
+import Layout from "../common/Layout"
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <Layout>
+            <div className='container py-5 mt-5'>
+                <div className='d-flex align-items-center justify-content-center'>
+                    <form >
+                        <div className='card border-0 shadow register'>
+                            <div className='card-body p-4'>
+                                <h3 className='border-bottom pb-3 mb-3'>Register</h3>
 
-        </div>
+                                <div className='mb-3'>
+                                    <label className='form-label' htmlFor="name">Name</label>
+                                    <input
+                                        type="text"
+                                        className={`form-control`}
+                                        placeholder='Name' />
+                                </div>
+
+
+                                <div className='mb-3'>
+                                    <label className='form-label' htmlFor="email">Email</label>
+                                    <input
+                                        type="text" className={`form-control`}
+                                        placeholder='Email' />
+
+                                </div>
+
+                                <div className='mb-3'>
+                                    <label className='form-label' htmlFor="password">Password</label>
+                                    <input
+                                        type="password"
+                                        className={`form-control`}
+                                        placeholder='Password' />
+                                </div>
+
+                                <div>
+                                    <button className='btn btn-primary w-100'>Register</button>
+                                </div>
+
+                                <div className='d-flex justify-content-center py-3'>
+                                    Already have account? &nbsp;<Link className='text-secondary' to={`/account/login`}> Login</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
